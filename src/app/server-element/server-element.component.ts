b3d845a1e45aec80ecefc84be302011e81e28760
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-09-05 13:11:03 +0200

    Introduction to input aliases

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -7,11 +7,10 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
